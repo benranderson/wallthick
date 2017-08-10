8d5b9602348830167da75bed92966191f2b46a8b
Ben Randerson <ben.m.randerson@gmail.com>
2017-08-10 13:42:11 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,55 @@
-# -*- coding: utf-8 -*-
-
+from __future__ import print_function
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import io
+import codecs
+import os
+import sys
+
+import wallthick
+
+here = os.path.abspath(os.path.dirname(__file__))
 
 
-with open('README.md') as f:
-    readme = f.read()
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
 
-with open('LICENCE') as f:
-    license = f.read()
+
+long_description = read('README.md')
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
 
 setup(
     name='wallthick',
-    version='0.0.0',
-    description='PD8010 wall thickness calculations',
-    long_description=readme,
+    version=wallthick.__version__,
     url='https://github.com/benranderson/wallthick',
+    license='MIT License',
     author='Ben Randerson',
     author_email='ben.m.randerson@gmail.com',
-    license=license,
+    description='PD8010 wall thickness calculations',
+    long_description=long_description,
+    keywords='wall thickness engineering pipelines',
+    packages=['wallthick'],
+    test_suite='tests',
+    tests_require=['pytest'],
+    cmdclass={'test': PyTest},
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Other Audience',
@@ -27,6 +59,7 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
     ],
-    keywords='wall thickness engineering pipelines',
-    packages=find_packages(exclude=('tests', 'docs'))
+    extras_require={
+        'testing': ['pytest'],
+    }
 )
--- wallthick/__init__.py
+++ wallthick/__init__.py
@@ -1,3 +1,5 @@
 from . import pd8010
 from .pd8010 import Pd8010
 from .inputs import Pipe, Process, Environment
+
+__version__ = '0.0.1'
