255553a2909d367700be9985f8ca1a18f409a8be
Ben Randerson <ben.m.randerson@gmail.com>
2018-02-28 19:41:28 +0000

    updated cli

--- wallthick/cli.py
+++ wallthick/cli.py
@@ -35,17 +35,17 @@ def main(inputs):
     '''Console script for wallthick.'''
     data = json.load(inputs)
     if all(param in data for param in req_inputs):
-        click.echo('Running wall thickness calculation...')
+        click.echo('🔩  Running PD 8010-2 wall thickness calculation...')
         pd = Pd8010(data)
-        click.echo(f'\nNominal Wall thicknesses')
+        click.echo(f'\nNominal Wall Thicknesses')
         click.echo(f'------------------------\n')
-        click.echo(f'Pressure Containment: {1000*pd.t_h:.3f} mm')
-        click.echo(f'Hydrostatic Collapse: {1000*pd.t_c:.3f} mm')
-        click.echo(f'Propagation buckling: {1000*pd.t_b:.3f} mm')
+        click.echo(f'Pressure Containment:\t{1000*pd.t_h:.3f} mm')
+        click.echo(f'Hydrostatic Collapse:\t{1000*pd.t_c:.3f} mm')
+        click.echo(f'Propagation Buckling:\t{1000*pd.t_b:.3f} mm')
         click.echo(f'\nTest Pressures')
         click.echo(f'--------------\n')
-        click.echo(f'Strength Test Pressure: {0.00001*pd.P_st:.1f} bar')
-        click.echo(f'Leak Test Pressure: {0.00001*pd.P_lt:.1f} bar')
+        click.echo(f'Strength Test Pressure:\t{0.00001*pd.P_st:.1f} bar')
+        click.echo(f'Leak Test Pressure:\t{0.00001*pd.P_lt:.1f} bar\n')
     else:
         click.echo(
             f'Check input data file includes all of the following: {req_inputs}')
